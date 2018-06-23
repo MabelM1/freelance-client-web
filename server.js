39ac38153a5b457dcdee87dcd2c0913cd539f880
Mabel <macmota@hotmail.com>
2018-06-23 17:28:00 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, () => {
+	console.log("Listening on port " + PORT);
+});
